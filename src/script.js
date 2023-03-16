c0ace02d40799c01ef28a204476976102eff1afa
lorries79 <gracesadler79@hotmail.com>
2023-03-16 19:09:32 +0100

    chaged to shecodes weather api

--- src/script.js
+++ src/script.js
@@ -68,8 +68,9 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiKey = "2558fcbe6d6a44b1e227ot72befa01f43";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.lon}&lat=${coordinates.lat}&key=${apiKey}&units=metric`;
+
   axios.get(apiUrl).then(displayForecast);
 }
 
